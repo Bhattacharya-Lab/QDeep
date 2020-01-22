08e6c4b8c8d1a53b4d5e898b4734b81e0fa7ca4a
mzsau <mzs0149@auburn.edu>
2020-01-22 11:51:36 -0600

    add

---
+++ configure.py
@@ -0,0 +1,15 @@
+import os, sys
+working_dir = os.getcwd()
+with open("QDeep.py", "rt") as input_file:
+	with open("QDeep_tmp.py", "wt") as output_file:
+		for line in input_file:
+			output_file.write(line.replace('change/to/your/current/directory', working_dir + '/'))
+os.system('mv QDeep_tmp.py QDeep.py')
+
+with open("QDeep.py", "rt") as input_file:
+        with open("QDeep_tmp.py", "wt") as output_file:
+                for line in input_file:
+                        output_file.write(line.replace('configured = 0', 'configured = 1'))
+os.system('mv QDeep_tmp.py QDeep.py')
+
+print('\nConfigured successfully!\n')
